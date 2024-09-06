37ab2959ed3c6674e3af07b612d77cc0960e3a15
ptr <ptr@factory-talk.com>
2024-09-06 15:22:46 +0700

    feat: add distribute and sold

--- contracts/Traceability.sol
+++ contracts/Traceability.sol
@@ -15,32 +15,20 @@ contract Traceability {
     }
 
     bytes32 product;
-    mapping (address => Info) stakeholders;
     mapping (address => bytes32) serials;
-    mapping (address => address) signed;
-    event Signed(address accepter, address indexed requester);
+    mapping (address => address) parties;
+    mapping (address => Info) stakeholders;
+    event Signed(address acceptor, address indexed requester);
 
-    constructor(bytes32 _productHash, bytes32 _serialHash, string memory _company) {
-        product = _productHash;
+    constructor(bytes32 _product, bytes32 _serial, string memory _company) {
+        product = _product;
+        serials[msg.sender] = _serial;
         stakeholders[msg.sender] = Info(_company, true, ROLE.MANUFACTURER);
-        serials[msg.sender] = _serialHash;
     }
 
-    modifier signAccept(address _requester) {
-        require(signed[msg.sender] == _requester, "Unathorized");
-        emit Signed(msg.sender, _requester);
+    modifier checkProduct(bytes32 _product) {
+        require(_product == product, "Wrong Product");
         _;
-    }
-
-    function signRequest(address _acceptor) internal {
-        require(stakeholders[msg.sender].exist, "Unathorized");
-        require(msg.sender != signed[_acceptor], "Unathorized");
-        require(msg.sender != _acceptor, "Unathorized");
-        signed[_acceptor] = msg.sender;
-    }
-    
-    function checkProduct(bytes32 _product) external view returns (bool) {
-        return _product == product;
     }
 
     modifier checkSerial(bytes32 _serial) {
@@ -48,7 +36,45 @@ contract Traceability {
         _;
     }
 
-    function setNewSerial(bytes32 _serial, bytes32 _newSerial) external checkSerial(_serial) {
-        serials[msg.sender] = _newSerial;
+    modifier signAccept(address _requester) {
+        require(parties[msg.sender] == _requester, "Unathorized");
+        emit Signed(msg.sender, _requester);
+        _;
+    }
+
+    function signRequest(
+        bytes32 _product, 
+        bytes32 _serial, 
+        bytes32 _shipment, 
+        address _acceptor
+    ) external checkProduct(_product) checkSerial(_serial) {
+        require(stakeholders[msg.sender].exist, "Unathorized");
+        require(msg.sender != parties[_acceptor], "Unathorized");
+        require(msg.sender != _acceptor, "Unathorized");
+        serials[_acceptor] = _shipment;
+        parties[_acceptor] = msg.sender;
+    }
+    
+    function confirmDistribute(
+        bytes32 _product, 
+        bytes32 _serial, 
+        address _requester, 
+        string calldata _company, 
+        ROLE _role
+    ) external checkProduct(_product) checkSerial(_serial) signAccept(_requester) {
+        require(stakeholders[msg.sender].role != ROLE.PHARMACY, "Unathorized");
+        require(_role != ROLE.MANUFACTURER, "Unathorized");
+        stakeholders[msg.sender] = Info(_company, true, _role);
+    }
+
+    function soldDrug(
+        bytes32 _product, 
+        bytes32 _serial, 
+        bytes32 _stock, 
+        address _customer
+    ) external checkProduct(_product) checkSerial(_serial) {
+        require(stakeholders[msg.sender].role == ROLE.PHARMACY, "Unathorized");
+        serials[msg.sender] = _stock;
+        parties[_customer] = msg.sender;
     }
 }
