2544500f64812d29f50f32ae44c843727045ea66
ptr <ptr@factory-talk.com>
2024-09-08 15:19:52 +0700

    refactor: distribute function

--- contracts/Traceability.sol
+++ contracts/Traceability.sol
@@ -8,73 +8,86 @@ contract Traceability {
         PHARMACY
     }
 
-    struct Info {
+    struct Stakeholder {
         string company;
         bool exist;
         ROLE role;
     }
 
     bytes32 product;
-    mapping (address => bytes32) serials;
+    bytes32 serialize;
+    mapping (address => Stakeholder) stakeholders;
+    mapping (address => bytes32) distributes;
     mapping (address => address) parties;
-    mapping (address => Info) stakeholders;
     event Signed(address acceptor, address indexed requester);
 
-    constructor(bytes32 _product, bytes32 _serials, string memory _company) {
+    constructor(
+        bytes32 _product, 
+        bytes32 _serialize, 
+        string memory _manufacturer
+    ) {
         product = _product;
-        serials[msg.sender] = _serials;
-        stakeholders[msg.sender] = Info(_company, true, ROLE.MANUFACTURER);
+        serialize = _serialize;
+        distributes[msg.sender] = _serialize;
+        stakeholders[msg.sender] = Stakeholder(_manufacturer, true, ROLE.MANUFACTURER);
     }
 
-    modifier checkProduct(bytes32 _product) {
-        require(_product == product, "Wrong Product");
+    modifier validate(bytes32 _product, bytes32 _serialize, bytes32 _distribute) {
+        require(_product == product, "Error: Wrong Product");
+        require(_serialize == serialize, "Error: Wrong Serialize");
+        require(_distribute == distributes[msg.sender], "Error: Wrong Distribution");
         _;
     }
 
-    modifier checkSerials(bytes32 _serials) {
-        require(_serials == serials[msg.sender], "Wrong Serials");
+    modifier auth(address _acceptor) {
+        require(stakeholders[msg.sender].exist, "Error: Unathorized");
+        require(msg.sender != _acceptor, "Error: Unathorized");
         _;
     }
 
-    modifier signAccept(address _requester) {
-        require(parties[msg.sender] == _requester, "Unathorized");
+    modifier sign(address _requester) {
+        require(parties[msg.sender] == _requester, "Error: Unathorized");
         emit Signed(msg.sender, _requester);
         _;
     }
 
-    function signRequest(
+    function shipmentRequest(
         bytes32 _product, 
-        bytes32 _serials, 
+        bytes32 _serialize, 
+        bytes32 _distribute, 
         bytes32 _shipment, 
-        address _acceptor
-    ) external checkProduct(_product) checkSerials(_serials) {
-        require(stakeholders[msg.sender].exist, "Unathorized");
-        require(msg.sender != parties[_acceptor], "Unathorized");
-        require(msg.sender != _acceptor, "Unathorized");
-        serials[_acceptor] = _shipment;
+        address _acceptor,
+        string calldata _company,
+        ROLE _role
+    ) external validate(_product, _serialize, _distribute) auth(_acceptor) {
+        require(stakeholders[msg.sender].role != ROLE.PHARMACY, "Error: Unathorized");
+        require(msg.sender != parties[_acceptor], "Error: Unathorized");
+        require(_role != ROLE.MANUFACTURER, "Error: Unathorized");
+        distributes[_acceptor] = _shipment;
         parties[_acceptor] = msg.sender;
+        stakeholders[_acceptor] = Stakeholder(_company, false, _role);
     }
     
-    function confirmDistribute(
+    function shipmentConfirm(
         bytes32 _product, 
-        bytes32 _serials, 
-        address _requester, 
-        string calldata _company, 
-        ROLE _role
-    ) external checkProduct(_product) checkSerials(_serials) signAccept(_requester) {
-        require(stakeholders[msg.sender].role != ROLE.PHARMACY, "Unathorized");
-        require(_role != ROLE.MANUFACTURER, "Unathorized");
-        stakeholders[msg.sender] = Info(_company, true, _role);
+        bytes32 _serialize, 
+        bytes32 _distribute, 
+        address _requester
+    ) external validate(_product, _serialize, _distribute) sign(_requester) {
+        stakeholders[msg.sender].exist = true;
     }
 
-    function soldDrug(
+    function sellDrug(
         bytes32 _product, 
-        bytes32 _serials, 
+        bytes32 _serialize, 
         bytes32 _stock, 
+        bytes32 _remain, 
+        bytes32 _sold, 
         address _patient
-    ) external checkProduct(_product) checkSerials(_serials) {
-        require(stakeholders[msg.sender].role == ROLE.PHARMACY, "Unathorized");
-        serials[msg.sender] = _stock;
+    ) external validate(_product, _serialize, _stock) auth(_patient) {
+        require(stakeholders[msg.sender].role == ROLE.PHARMACY, "Error: Unathorized");
+        distributes[msg.sender] = _remain;
+        distributes[_patient] = _sold;
         parties[_patient] = msg.sender;
     }
 }
