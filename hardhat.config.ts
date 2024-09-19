f7cea91c8c4433c71d5f5a23a19add4eab204585
Parin T. <parin.t@linecorp.com>
2024-09-19 09:41:57 +0700

    chore: add contract optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: '0.8.24',
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
 };
 
 export default config;
